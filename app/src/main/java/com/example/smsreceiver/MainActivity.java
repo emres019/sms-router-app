075f218d977f6637ae759530ba450400de91dace
Emre Sarıncıoğlu <emresrncgl@gmail.com>
2023-01-21 22:23:22 +0300

    Added save button on click listener, shared preferences keys and text field empty error message

--- app/src/main/java/com/example/smsreceiver/MainActivity.java
+++ app/src/main/java/com/example/smsreceiver/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.smsreceiver;
 
 import android.Manifest;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
@@ -22,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    private TextInputLayout mTfFrom;
+    private TextInputLayout mTfTo;
+    private TextInputLayout mTfPattern;
+
     private final ActivityResultLauncher<String[]> mSmsPermissionLauncher = registerForActivityResult(
             new ActivityResultContracts.RequestMultiplePermissions(),
             new ActivityResultCallback<Map<String, Boolean>>() {
@@ -30,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
                     if (Boolean.FALSE.equals(areGranted.get(Manifest.permission.RECEIVE_SMS)) ||
                             Boolean.FALSE.equals(areGranted.get(Manifest.permission.SEND_SMS))) {
                         Snackbar.make(findViewById(android.R.id.content),
-                                getString(R.string.snackbar_permission_text),
+                                getString(R.string.snack_bar_permission_text),
                                 Snackbar.LENGTH_INDEFINITE)
                                 .setAction(android.R.string.ok, v -> mSmsPermissionLauncher.launch(new String[] {
                                         Manifest.permission.RECEIVE_SMS,
@@ -73,6 +79,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Assign text fields
+        mTfFrom = findViewById(R.id.tf_from);
+        mTfTo = findViewById(R.id.tf_to);
+        mTfPattern = findViewById(R.id.tf_pattern);
+
         // Request necessary permissions
         mSmsPermissionLauncher.launch(new String[] {
                 Manifest.permission.RECEIVE_SMS,
@@ -84,5 +95,39 @@ public class MainActivity extends AppCompatActivity {
                 .setStartIconOnClickListener(v -> mContactPickerLauncher.launch(null));
         this.<TextInputLayout>findViewById(R.id.tf_to)
                 .setStartIconOnClickListener(v -> mContactPickerLauncher.launch(null));
+
+        // Save sender, receiver and message when save button is clicked
+        findViewById(R.id.btn_save).setOnClickListener(this::btnSaveOnClick);
+    }
+
+    private void btnSaveOnClick(View btn) {
+        String from = mTfFrom.getEditText().getText().toString().trim();
+        String to = mTfTo.getEditText().getText().toString().trim();
+        String pattern = mTfPattern.getEditText().getText().toString().trim();
+
+        if (from.isEmpty()) {
+            mTfFrom.setError(getString(R.string.error_text_empty));
+            return;
+        }
+
+        if (to.isEmpty()) {
+            mTfTo.setError(getString(R.string.error_text_empty));
+            return;
+        }
+
+        if (pattern.isEmpty()) {
+            mTfPattern.setError(getString(R.string.error_text_empty));
+            return;
+        }
+
+        // Save data to shared preferences
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putString(getString(R.string.saved_from_key), from);
+        editor.putString(getString(R.string.saved_to_key), to);
+        editor.putString(getString(R.string.saved_pattern_key), pattern);
+
+        editor.apply();
     }
 }

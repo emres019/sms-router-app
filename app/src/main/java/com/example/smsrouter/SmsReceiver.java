8b260a6a4c86d822d6f1a72eed517e1fc2c926db
Emre Sarıncıoğlu <emresrncgl@gmail.com>
2023-03-22 12:04:29 +0300

    Added try catch block

--- app/src/main/java/com/example/smsrouter/SmsReceiver.java
+++ app/src/main/java/com/example/smsrouter/SmsReceiver.java
@@ -5,12 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.os.Bundle;
 import android.provider.Telephony;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresPermission;
@@ -21,46 +19,30 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SmsReceiver extends BroadcastReceiver {
-
-    private static final String LOG_TAG = SmsReceiver.class.getSimpleName();
-
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        SharedPreferences prefs = context.getSharedPreferences(context.getString(R.string.preferences_file_key), Context.MODE_PRIVATE);
-        if (intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION) &&
-                prefs.getBoolean(context.getString(R.string.saved_enabled_key), false)) {
-            // Get the SMS attributes
-            Bundle bundle = intent.getExtras();
-            String format = bundle.getString("format");
-
-            byte[][] pdus = (byte[][]) bundle.get("pdus");
-            if (pdus != null) {
-                // Log sms receive
-                Log.i(LOG_TAG, "onReceive: Got " + pdus.length + " SMS");
-
+        try {
+            SharedPreferences prefs = context.getSharedPreferences(context.getString(R.string.preferences_file_key), Context.MODE_PRIVATE);
+            if (intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION) &&
+                    prefs.getBoolean(context.getString(R.string.saved_enabled_key), false)) {
                 // Get shared preferences data
                 String sender = prefs.getString(context.getString(R.string.saved_sender_key), null);
                 if (sender == null) {
-                    Log.e(LOG_TAG, "onReceive: Sender is null");
                     return;
                 }
 
                 String receiver = prefs.getString(context.getString(R.string.saved_receiver_key), null);
                 if (receiver == null) {
-                    Log.e(LOG_TAG, "onReceive: Receiver is null");
                     return;
                 }
 
                 String messagePattern = prefs.getString(context.getString(R.string.saved_pattern_key), null);
                 if (messagePattern == null) {
-                    Log.e(LOG_TAG, "onReceive: Pattern is null");
                     return;
                 }
 
-                for (byte[] bytes : pdus) {
-                    SmsMessage message = SmsMessage.createFromPdu(bytes, format);
-
+                for (SmsMessage message : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
                     String strippedSender = PhoneNumberUtils.stripSeparators(sender);
                     String strippedMessageAddress = PhoneNumberUtils.stripSeparators(message.getOriginatingAddress());
                     boolean isMessageFromSender = (PhoneNumberUtils.isGlobalPhoneNumber(strippedMessageAddress)
@@ -78,12 +60,13 @@ public class SmsReceiver extends BroadcastReceiver {
                     }
                 }
             }
+        } catch (Exception e) {
+
         }
     }
 
     /**
      * Sends SMS message
-     *
      * @param message  Message to send
      * @param receiver Receiver phone number / address
      * @see SmsManager
